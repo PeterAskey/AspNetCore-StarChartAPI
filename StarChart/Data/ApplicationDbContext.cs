37c8377ac69759c519021f3f4d0b8e76527c6f45
Peter Askey <peter_askey@waters.com>
2018-11-26 12:11:48 +0000

    Stage 3

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -4,8 +4,10 @@ namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
     }
 }
+
